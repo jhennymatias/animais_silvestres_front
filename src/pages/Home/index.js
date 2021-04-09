0be6377646814b4b77aa6ee321d16da21da1bea3
jhennymatias <jhennifercristinematias@gmail.com>
2021-04-09 12:52:58 -0300

    estrutura de pastas feitas

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+function Home() {
+
+    return (
+        <div className="home-container">
+            Home
+        </div>
+    );
+}
+
+export default Home;
